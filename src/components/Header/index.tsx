218fcff4725930f55892a1d06d4fdb7a4dced7cb
LucasVTK <lucazim.vtk@gmail.com>
2025-05-03 16:43:31 -0300

    Implementa Redux com carrinho e Redux Toolkit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,20 @@
 import * as S from './styles'
-
-import { Produto } from '../../App'
-
 import cesta from '../../assets/cesta.png'
+import { Produto } from '../../App'
 import { paraReal } from '../Produto'
 
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store'
+
 type Props = {
-  itensNoCarrinho: Produto[]
   favoritos: Produto[]
 }
 
-const Header = ({ itensNoCarrinho, favoritos }: Props) => {
+const Header = ({ favoritos }: Props) => {
+  const itensNoCarrinho = useSelector(
+    (state: RootState) => state.carrinho.itens
+  )
+
   const valorTotal = itensNoCarrinho.reduce((acc, item) => {
     acc += item.preco
     return acc
